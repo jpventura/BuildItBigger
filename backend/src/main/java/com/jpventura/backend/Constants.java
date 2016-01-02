4f15d0a14e650532169cb3b2dbe722f3847f6a51
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2016-01-02 19:31:22 -0200

    feat: Create Google Endpoints module.

---
+++ backend/src/main/java/com/jpventura/backend/Constants.java
@@ -0,0 +1,11 @@
+package com.jpventura.backend;
+
+import com.google.api.server.spi.Constant;
+
+public class Constants {
+    public static final String API_EMAIL_SCOPE = Constant.API_EMAIL_SCOPE;
+    public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
+    public static final String ANDROID_CLIENT_ID = "Your Android client ID credential";
+    public static final String OWNER_DOMAIN = "your_app_id.appspot.com";
+    public static final String WEB_CLIENT_ID = "Your OAuth web client ID credential";
+}
---
+++ backend/src/main/java/com/jpventura/backend/MyEndpoint.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015 JP Ventura
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jpventura.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.jpventura.ChuckNorrisDatabase;
+import com.jpventura.IChuckNorrisDatabase;
+
+@Api(
+        name = "jokes",
+        version = "v1",
+        clientIds = {
+                Constants.ANDROID_CLIENT_ID,
+                Constants.API_EXPLORER_CLIENT_ID,
+                Constants.WEB_CLIENT_ID
+        },
+        scopes = {
+                Constants.API_EMAIL_SCOPE
+        },
+        namespace = @ApiNamespace(
+                ownerDomain = Constants.OWNER_DOMAIN,
+                ownerName = Constants.OWNER_DOMAIN,
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+    private IChuckNorrisDatabase mChuckNorrisDatabase;
+
+    public MyEndpoint() {
+        mChuckNorrisDatabase = ChuckNorrisDatabase.getInstance();
+    }
+
+    @ApiMethod(name = "getJoke", path = "jokes", httpMethod = ApiMethod.HttpMethod.GET)
+    public JokeBean getJoke() {
+        return new JokeBean(mChuckNorrisDatabase.getRandomJoke().value);
+    }
+}
