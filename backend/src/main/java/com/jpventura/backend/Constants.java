8adb6d339a5fc167e8124e8c80d7f17dd27383c5
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2016-01-03 20:25:05 -0200

    feat: Add production keys.

--- backend/src/main/java/com/jpventura/backend/Constants.java
+++ backend/src/main/java/com/jpventura/backend/Constants.java
@@ -5,7 +5,7 @@ import com.google.api.server.spi.Constant;
 public class Constants {
     public static final String API_EMAIL_SCOPE = Constant.API_EMAIL_SCOPE;
     public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
-    public static final String ANDROID_CLIENT_ID = "Your Android client ID credential";
-    public static final String OWNER_DOMAIN = "your_app_id.appspot.com";
-    public static final String WEB_CLIENT_ID = "Your OAuth web client ID credential";
+    public static final String ANDROID_CLIENT_ID = "280863524374-293ph7d5ruem5493kkithp9e0sjh0vld.apps.googleusercontent.com";
+    public static final String OWNER_DOMAIN = "build-it-bigger-1179.appspot.com";
+    public static final String WEB_CLIENT_ID = "280863524374-2h5fheidg07o7ctb4c2o7i1qfpmko1ft.apps.googleusercontent.com";
 }
